0a47664cb6e420662e0634e0718650515ee2cbe6
Erik Bülow <eriklgb@gmail.com>
2026-01-16 17:00:23 +0100

    a lot of changes

---
+++ render.R
@@ -0,0 +1,109 @@
+# Detta skript används för att rendera och uppdatera hela sitden
+
+library(tidyverse)
+library(vvcanvas)
+
+# Rendera siten lokalt
+quarto::quarto_render(".")
+
+# publicera till GitHub Pages (gh-pages branch)
+system("quarto publish gh-pages --no-prompt")
+
+
+# Export handouts to Canvas -----------------------------------------
+
+# NOTE: Needs special permission with API-keys etc
+# (not possibke to do this if you are not the teacher :-))
+library(vvcanvas)
+
+# Identify folder
+Eriks_material <- get_course_files(canvas, sta220$id) |>
+  as_tibble() |>
+  filter(grepl("swedish_personal_id", display_name)) |>
+  pluck("folder_id")
+
+# Upload/update handouts
+upload_folder_file(
+  canvas,
+  folder_id = Eriks_material,
+  file_name = "_site/handouts.pdf"
+)
+
+
+# Publicera läslista -----------------------------------------------------
+
+canvas <- canvas_authenticate()
+sta220 <- get_courses(canvas) |>
+  as_tibble() |>
+  filter(startsWith(course_code, "STA220"))
+
+# Befintliga filer
+canvas_pdfs <- get_course_files(canvas, sta220$id) |>
+  as_tibble() |>
+  select(filename, url)
+
+pdfs <-
+  qs2::qs_read("cache/lectures.qs2") |>
+  select(Session, literature) |>
+  mutate(literature = strsplit(literature, "], [", fixed = TRUE)) |>
+  unnest(literature) |>
+  mutate(
+    literature = gsub("(\\[?\\@)(\\w*)(.*)", "\\2", literature, perl = TRUE),
+    filename = paste0(literature, ".pdf"),
+    upload_from = paste0("articles/", literature, ".pdf")
+  ) |>
+  filter(
+    upload_from %in% dir("articles", full.names = TRUE)
+  ) |>
+  left_join(canvas_pdfs)
+
+
+# Upload PDF:s which are not already in Canvas
+pdfs_to_upload <-
+  pdfs |>
+  filter(is.na(url)) |>
+  pluck("filename")
+if (length(pdfs_to_upload) > 0) {
+  walk(pdfs_to_upload, \(x) {
+    upload_folder_file(
+      canvas,
+      folder_id = Eriks_material,
+      file_name = x
+    )
+  })
+}
+
+# tabell att presentera
+html_table <-
+  pdfs |>
+  mutate(
+    url = sub("\\?.*$", "", url),
+    Session,
+    download = glue::glue("<a href='{url}'>{literature}</a>")
+  ) |>
+  mutate(Session = if_else(duplicated(Session), "", Session)) |>
+  select(Session, download) |>
+  knitr::kable(format = "html", escape = FALSE)
+
+# HTML body att lägga in
+
+body <- '
+Note that some required reading consints of web pages. 
+Check the full reading list in the
+<a href="https://sta220.github.io/documentation/">course plan</a>
+<p>
+You may also acces the referenced articles by links provided in the reference list.
+If you are not able to do that, static PDF:s are found below.
+' |>
+  paste(html_table, sep = "\n\n")
+
+# Uppdatera sidan
+update_page(
+  canvas,
+  sta220$id,
+  "851450",
+  page_params = list(
+    title = "PDF:s to read",
+    body = body
+  )
+)
---
+++ scheduele.R
@@ -0,0 +1,94 @@
+suppressPackageStartupMessages(library(tidyverse))
+library(gt)
+
+# Session codes
+actshort <- c(
+  "Lecture" = "L",
+  "Computer training" = "CS",
+  "Compulsory, Seminar" = "S",
+  "Compulsory, Computer training" = "CSM"
+)
+
+# Find lecture titles based on lecture slides
+lectures <-
+  tibble(files = dir(".", "E(L|CS)[0-9]*.qmd")) |>
+  mutate(
+    Session = gsub("\\.qmd", "", files),
+    title = map_chr(files, \(x) rmarkdown::yaml_front_matter(x)$title),
+    title = gsub("([A-Z0-9]*: )(\\w*)", "\\2", title),
+    subtitle = map_chr(files, \(x) rmarkdown::yaml_front_matter(x)$subtitle),
+    literature = map_chr(files, \(x) rmarkdown::yaml_front_matter(x)$reading),
+    url = sprintf(
+      # "[%s](https://sta220.github.io/documentation/%s.html)",
+      "[%s](%s.qmd)",
+      Session,
+      Session
+    )
+  ) |>
+  select(-files)
+qs2::qs_save(lectures, "cache/lectures.qs2")
+
+
+# Based on recent export from TimeEdit
+readxl::read_excel("schema.xlsx") |>
+  filter(
+    grepl("STA220", Course),
+    grepl("Erik", Teacher),
+    !grepl("Digital Exam|Retake", Activity),
+    # Hårdkodar bort session där jag hjälper Anna men inte har så mkt ansvar
+    !(grepl("Anna", Teacher) & Activity == "Computer training")
+  ) |>
+  transmute(
+    Week,
+    Weekday = factor(Weekday, c("Monday", "Wednesday"), c("Mon", "Wed")),
+    # Date = `Begin date`,
+    Time = paste(`Begin time`, `End time`, sep = "-"),
+    AGE = if_else(grepl("Anna", Teacher), "X", ""),
+    Activity,
+  ) |>
+  group_by(Activity) |>
+  mutate(Session = paste0("E", actshort[Activity], row_number())) |>
+  ungroup() |>
+  left_join(lectures, "Session") |>
+  mutate(Session = coalesce(url, Session)) |>
+  select(-url) |>
+  group_by(Week) |>
+  mutate(Weekday = if_else(duplicated(Weekday), "", Weekday)) |>
+  gt() |>
+  tab_style(
+    style = cell_fill(color = "lightgreen"),
+    locations = cells_body(
+      rows = grepl("Lecture", Activity),
+      columns = Activity
+    )
+  ) |>
+  tab_style(
+    style = cell_fill(color = "lightblue"),
+    locations = cells_body(
+      rows = grepl("Seminar", Activity),
+      columns = Activity
+    )
+  ) |>
+  tab_style(
+    style = cell_fill(color = "orange"),
+    locations = cells_body(
+      rows = grepl("Compulsory, Computer training", Activity),
+      columns = Activity
+    )
+  ) |>
+  tab_style(
+    style = cell_fill(color = "yellow"),
+    locations = cells_body(
+      rows = grepl("^Computer training", Activity),
+      columns = Activity
+    )
+  ) |>
+  sub_missing(missing_text = "") |>
+  fmt_markdown(columns = c(Session, literature)) |>
+  tab_options(table.layout = "auto") |>
+  tab_options(
+    container.overflow.x = TRUE,
+    table.font.size = px(11)
+  ) |>
+  opt_css("th, td { white-space: normal; }") |>
+  qs2::qs_save("cache/gt_schedule.qs2")
